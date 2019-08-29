337fa70acc58aa856e33f8a86de80e6252d979e2
Jenee Alberg <JAlberg@dfta.aging.nyc.gov>
2019-08-29 11:22:28 -0400

    End of section 10

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { MemberCardComponent} from './members/member-card/member-card.component'
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver.';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 
 export function tokenGetter(){
   return localStorage.getItem('token');
@@ -33,8 +36,8 @@ export function tokenGetter(){
 
 @NgModule({
   declarations: [
-    AppComponent,  NavComponent, HomeComponent, RegisterComponent, 
-    MemberListComponent, ListsComponent, MessagesComponent, MemberCardComponent, MemberDetailComponent
+    AppComponent,  NavComponent, HomeComponent, RegisterComponent,
+    MemberListComponent, ListsComponent, MessagesComponent, MemberCardComponent, MemberDetailComponent, MemberEditComponent
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
@@ -54,7 +57,9 @@ export function tokenGetter(){
     AuthGuard,
     UserService,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges
   ],
   bootstrap: [AppComponent],
 })
